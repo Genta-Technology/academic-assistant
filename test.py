755ec0590d46e1e50e1c3d3f107aa0fdc391ad19
Evintkoo <99308658+Evintkoo@users.noreply.github.com>
2023-09-15 20:29:09 -0700

    Add weaviate functionality

---
+++ test.py
@@ -0,0 +1,5 @@
+from utils import get_abstract
+
+print(get_abstract(input_str="how to make an abstract",
+                   weaviate_url='https://genta-academic-assistant-cluster-4vw2zql4.weaviate.network',
+                   openai_api_token="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"))
--- utils.py
+++ utils.py
@@ -1,8 +1,6 @@
-import numpy as np
-from numpy import dot
-from numpy.linalg import norm
-import openai 
-from openai.embeddings_utils import cosine_similarity
+import weaviate
+import openai
+
 
 def open_ai_embeddings(input_str:str, api_token:str):
     # use the api token
@@ -13,12 +11,20 @@ def open_ai_embeddings(input_str:str, api_token:str):
     embedding = openai.Embedding.create(input=input_str, model=model_id)['data'][0]['embedding']
     return embedding
 
-def top_n_similarity_index(n:int, main_vector:np.array, vector_list:np.array):
-    similarity_list = [cosine_similarity(main_vector, v) for v in vector_list]
-    similarity_list = np.argsort(similarity_list)[-n:]
-    return similarity_list
-
-def get_embedding_similarity_index(n:int, input_str:str, embedding_list:list, api_token:str):
-    embedded_text = open_ai_embeddings(input_str, api_token=api_token)
-    top_index = top_n_similarity_index(n=n, main_vector=embedded_text, vector_list=embedding_list)
-    return top_index
+def get_abstract(input_str:str, weaviate_url: str, openai_api_token:str, n:int = 5):
+    input_emb = open_ai_embeddings(input_str, openai_api_token)
+    
+    client = weaviate.Client(
+        url = weaviate_url,
+        additional_headers={
+            "X-OpenAI-Api-Key": openai_api_token
+        }
+    )
+    response = (
+        client.query
+        .get("Paper", ["dOI", "authors", "abstract", "date"])
+        .with_near_vector({'vector': input_emb})
+        .with_limit(n)
+        .do()
+    )
+    return response["data"]["Get"]["Paper"]
