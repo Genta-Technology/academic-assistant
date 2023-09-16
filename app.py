ad1e78f780c4c4bdfd96c4e7e45245b92802b0e2
IceZone123 <mhilmya04@gmail.com>
2023-09-16 14:05:20 -0700

    added message when user hasn't add their token

--- app.py
+++ app.py
@@ -7,10 +7,7 @@ import streamlit as st
 st.title("GENTA")
 
 with st.sidebar:
-    st.text_input("**Please enter token**", placeholder="Token", key="token")
-
-if "token" not in st.session_state:
-    st.session_state.token = ""
+    st.text_input("Please enter token", placeholder="Token", key="token")
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -19,15 +16,18 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-if prompt := st.chat_input("Wassup"):
-    with st.chat_message("user"):
-        st.markdown(prompt)
+if st.session_state.token:
+    if prompt := st.chat_input("Wassup"):
+        with st.chat_message("user"):
+            st.markdown(prompt)
 
-    st.session_state.messages.append({"role": "user", "content": prompt})
+        st.session_state.messages.append({"role": "user", "content": prompt})
 
-    response = f"Echo : {prompt}"
+        response = f"Echo : {prompt}"
 
-    with st.chat_message("assistant"):
-        st.markdown(response)
+        with st.chat_message("assistant"):
+            st.markdown(response)
 
-    st.session_state.messages.append({"role": "assistant", "content": response})
+        st.session_state.messages.append({"role": "assistant", "content": response})
+else:
+    st.write("Please enter your OpenAI API TOKEN first in the sidebar to use this application")
