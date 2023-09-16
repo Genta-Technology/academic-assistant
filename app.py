cd0e40c76d0fa42a029cdbccc4153bd83354022d
Restyled.io <commits@restyled.io>
2023-09-16 21:06:07 +0000

    Restyled by black

--- app.py
+++ app.py
@@ -28,8 +28,8 @@ if st.session_state.token:
         with st.chat_message("assistant"):
             st.markdown(response)
 
-        st.session_state.messages.append(
-            {"role": "assistant", "content": response})
+        st.session_state.messages.append({"role": "assistant", "content": response})
 else:
     st.write(
-        "Please enter your OpenAI API TOKEN first in the sidebar to use this application")
+        "Please enter your OpenAI API TOKEN first in the sidebar to use this application"
+    )
