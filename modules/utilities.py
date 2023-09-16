7bb3c00283668db415c9c19735aee9559edb23fa
Restyled.io <commits@restyled.io>
2023-09-16 04:43:40 +0000

    Restyled by reorder-python-imports

--- modules/utilities.py
+++ modules/utilities.py
@@ -1,12 +1,12 @@
 """
 A collection of utility functions.
 """
-
 import os
 
 import openai
 import weaviate
-from dotenv import find_dotenv, load_dotenv
+from dotenv import find_dotenv
+from dotenv import load_dotenv
 
 
 def open_ai_embeddings(input_str: str, api_token: str):
