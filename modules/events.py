63d8b5721169c5bcb85136b3f93bbaca9cb6f7c3
Rifky Bujana Bisri <rifkybujanabisri@gmail.com>
2023-09-15 22:07:49 -0700

    fix session state

--- modules/events.py
+++ modules/events.py
@@ -1,8 +1,11 @@
+import streamlit as st
+
 from .utilities import get_abstract
 
-def trigger_event(user_message, user_open_api_token, session):
-    if not session:
-        abstract_list = get_abstract(input_str=user_message, 
+def trigger_event(user_open_api_token):
+    # check if there is system in session state
+    if any(['system' in state.keys() for state in st.session_state.messages]):
+        abstract_list = get_abstract(input_str=st.session_state.messages[-1]['content'], 
                                      weaviate_url='https://genta-academic-assistant-cluster-4vw2zql4.weaviate.network',
                                      openai_api_token=user_open_api_token)
     else:
@@ -11,4 +14,4 @@ def trigger_event(user_message, user_open_api_token, session):
     """
     user_message: list(dict) -> [{"role": "system" or "assistant" or "user", "content": str}]
     """
-    new_user_message = ask_gpt(user_open_api_token, user_message, abstract_list)
+    new_user_message = ask_gpt(user_open_api_token, st.session_state.messages, abstract_list)
