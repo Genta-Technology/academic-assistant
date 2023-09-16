d64177265c0a9613e7656abc1cfc7766baa2c46a
Alifais Farrel Ramdhani <farrellabindonesia@gmail.com>
2023-09-16 13:46:22 -0700

    Update events.py
    add new_question() for determining to search for abstract or not

--- modules/events.py
+++ modules/events.py
@@ -3,11 +3,12 @@ import streamlit as st
 from .utilities import get_abstract
 
 def trigger_event(user_open_api_token):
-    # check if there is system in session state
-    if any(['system' in state.keys() for state in st.session_state.messages]):
-        abstract_list = get_abstract(input_str=st.session_state.messages[-1]['content'], 
-                                     weaviate_url='https://genta-academic-assistant-cluster-4vw2zql4.weaviate.network',
-                                     openai_api_token=user_open_api_token)
+    # check if the question need source
+    session_message = new_question(user_open_api_token, st.session_state.messages)
+    if session_message.lower() != "empty":
+        abstract_list = get_abstract(input_str=session_message, 
+                                        weaviate_url='https://genta-academic-assistant-cluster-4vw2zql4.weaviate.network',
+                                        openai_api_token=user_open_api_token)
     else:
         abstract_list = []
     # go to function from farrel
