fef5c53ef1cbaa9e30c97c75c06052dc80d9afce
Alifais Farrel Ramdhani <farrellabindonesia@gmail.com>
2023-09-16 13:51:52 -0700

    Update events.py

--- modules/events.py
+++ modules/events.py
@@ -1,6 +1,10 @@
+"""
+main python file for events and handling communication to gpt and get abstract
+"""
+
 import streamlit as st
 
-from .prompt import ask_gpt
+from .prompt import ask_gpt, new_question
 from .utilities import get_abstract
 
 def trigger_event(user_open_api_token):
@@ -13,9 +17,9 @@ def trigger_event(user_open_api_token):
     # check if the question need source
     session_message = new_question(user_open_api_token, st.session_state.messages)
     if session_message.lower() != "empty":
-        abstract_list = get_abstract(input_str=session_message, 
-                                        weaviate_url='https://genta-academic-assistant-cluster-4vw2zql4.weaviate.network',
-                                        openai_api_token=user_open_api_token)
+        abstract_list = get_abstract(input_str=session_message,
+                                    weaviate_url='https://genta-academic-assistant-cluster-4vw2zql4.weaviate.network',
+                                    openai_api_token=user_open_api_token)
     else:
         abstract_list = []
     # go to function from farrel
