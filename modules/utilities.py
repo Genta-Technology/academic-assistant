98188a6e4534da56ab9febb13cc2b5ca652fe8a1
Restyled.io <commits@restyled.io>
2023-09-16 03:56:31 +0000

    Restyled by yapf

--- modules/utilities.py
+++ modules/utilities.py
@@ -24,15 +24,15 @@ def open_ai_embeddings(input_str: str, api_token: str):
 
     # convert text to embedding
     model_id = "text-embedding-ada-002"
-    embedding = openai.Embedding.create(input=input_str, model=model_id)["data"][0][
-        "embedding"
-    ]
+    embedding = openai.Embedding.create(input=input_str,
+                                        model=model_id)["data"][0]["embedding"]
     return embedding
 
 
-def get_abstract(
-    input_str: str, weaviate_url: str, openai_api_token: str, top_n: int = 5
-):
+def get_abstract(input_str: str,
+                 weaviate_url: str,
+                 openai_api_token: str,
+                 top_n: int = 5):
     """Get the abstract of the top n papers similar to the input string.
 
     :param input_str: Input string.
@@ -55,12 +55,11 @@ def get_abstract(
     input_emb = open_ai_embeddings(input_str, openai_api_token)
 
     client = weaviate.Client(
-        url=weaviate_url, additional_headers={"X-OpenAI-Api-Key": openai_api_token}
-    )
-    response = (
-        client.query.get("Paper", ["dOI", "authors", "abstract", "date"])
-        .with_near_vector({"vector": input_emb})
-        .with_limit(top_n)
-        .do()
-    )
+        url=weaviate_url,
+        additional_headers={"X-OpenAI-Api-Key": openai_api_token})
+    response = (client.query.get(
+        "Paper", ["dOI", "authors", "abstract", "date"]).with_near_vector({
+            "vector":
+            input_emb
+        }).with_limit(top_n).do())
     return response["data"]["Get"]["Paper"]
