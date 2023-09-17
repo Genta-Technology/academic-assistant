f2037c6580c35474d99e84014022aa65407efb42
IceZone123 <mhilmya04@gmail.com>
2023-09-16 22:35:57 -0700

    Decorated the UI and also added a dropdown box

--- app.py
+++ app.py
@@ -4,11 +4,20 @@ FILE is for UI
 
 import streamlit as st
 
+from PIL import Image
 from modules.utilities import validate_openai_api_key
 
-st.title("GENTA - Academic Assitant")
-st.write("Genta Acadmic Assistant is a tool to help you" +
-         "find the right research paper for your research.")
+
+
+
+
+TITLE =( '<link rel="stylesheet" type="text/css"'+
+'href="http://fonts.googleapis.com/css?family=Ubuntu:regular,bold&subset=Latin">' + 
+'<H1 style="font-family:Ubuntu; font-size: 30px; font-weight:bold;">' +
+'<span style="font-size = 40px;">GENTA</span> - Academic Assitant</H1>')
+st.markdown(TITLE, unsafe_allow_html=True)
+st.markdown("""Genta Acadmic Assistant is a tool to help you""" +
+""" find the right research paper for your research.""")
 
 def on_token_change():
     """
@@ -23,6 +32,12 @@ def on_token_change():
         del st.session_state.token
 
 with st.sidebar:
+    col1, col2, col3 = st.columns(3)
+    img = Image.open("Extension.png")
+    with col2:
+        st.image(img, use_column_width="always")
+    BOX = '<style>#input {box-shadow: 10px 5px 5px black;}</style>'
+    st.markdown(BOX, unsafe_allow_html=True)
     st.text_input("Please enter token", placeholder="Token", key="token", on_change=on_token_change)
 
 if "messages" not in st.session_state:
@@ -36,6 +51,10 @@ if st.session_state.token:
     if prompt := st.chat_input("Wassup"):
         with st.chat_message("user"):
             st.markdown(prompt)
+            with st.expander("sources:"):
+                st.write("test1")
+                st.write("test2")
+                st.write("test3")
 
         st.session_state.messages.append({"role": "user", "content": prompt})
 
@@ -45,5 +64,8 @@ if st.session_state.token:
             st.markdown(response)
 
         st.session_state.messages.append({"role": "assistant", "content": response})
+        
 else:
-    st.write("Please enter your OpenAI API TOKEN first in the sidebar to use this application")
+    TEXT = ('<p style="font-size: 18px; font-weight:bold; color: #FF1D2E; margin-top: 15px;">'+
+    'Please enter your OpenAI API TOKEN first in the sidebar to use this application</p>')
+    st.markdown(TEXT, unsafe_allow_html=True)
