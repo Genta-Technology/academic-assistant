e3992783cd9a07230206ca4d15740c141bc3f112
Restyled.io <commits@restyled.io>
2023-09-19 23:43:27 +0000

    Restyled by autopep8

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ st.set_page_config(**PAGE_CONFIG)
 
 TITLE = (
     '<link rel="stylesheet" type="text/css"' +
-    'href="http://fonts.googleapis.com/css?family=Ubuntu:regular,bold&subset=Latin">' + 
+    'href="http://fonts.googleapis.com/css?family=Ubuntu:regular,bold&subset=Latin">' +
     '<H1 style="font-family:Ubuntu; font-size: 30px; font-weight:bold;">' +
     '<span style="font-size = 40px;">GENTA</span> - Academic Assistant</H1>')
 
@@ -72,8 +72,8 @@ if (len(st.session_state.messages) <= 8
 
         response, docs = trigger_event(
             st.session_state.token
-            if "token" in st.session_state \
-                and validate_openai_api_key(st.session_state.token)
+            if "token" in st.session_state
+            and validate_openai_api_key(st.session_state.token)
             else env["OPENAI_API_KEY"]
         )
 
@@ -94,8 +94,8 @@ if (len(st.session_state.messages) <= 8
 else:
     TEXT = (
         '<p style="font-size: 18px; font-weight:bold; color: #FF1D2E; margin-top: 15px;">' +
-        "Limit exceeded, refresh the page to start a new conversation or insert your own" + \
-        " <a href='https://platform.openai.com/account/api-keys'>OpenAI API key</a> " + \
+        "Limit exceeded, refresh the page to start a new conversation or insert your own" +
+        " <a href='https://platform.openai.com/account/api-keys'>OpenAI API key</a> " +
         "in the sidebar.</p>"
     )
     st.markdown(TEXT, unsafe_allow_html=True)
